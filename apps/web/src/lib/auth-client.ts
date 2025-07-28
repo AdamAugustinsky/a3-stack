52ce508c923d1dff84304758f21b75c376a0337b
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-07-28 20:47:30 -0300

    logs

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { PUBLIC_SERVER_URL } from "$env/static/public";
 import { createAuthClient } from "better-auth/svelte";
 
+console.log("AUTH PUBLIC_SERVER_URL", PUBLIC_SERVER_URL);
 export const authClient = createAuthClient({
-	baseURL: PUBLIC_SERVER_URL,
+  baseURL: PUBLIC_SERVER_URL,
 });
