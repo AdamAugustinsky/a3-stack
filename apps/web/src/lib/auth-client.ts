0ab119c4ed9350c0dcd60ed8fad43bb2e3b2091c
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-07-29 18:06:04 -0300

    sidebar layout and server side protected routes

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,6 @@
 import { PUBLIC_SERVER_URL } from "$env/static/public";
 import { createAuthClient } from "better-auth/svelte";
 
-console.log("AUTH PUBLIC_SERVER_URL", PUBLIC_SERVER_URL);
 export const authClient = createAuthClient({
   baseURL: PUBLIC_SERVER_URL,
 });
