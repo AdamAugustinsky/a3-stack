f6949716c599fab1613c250bc070ae4dc7f2c645
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-07-31 15:23:11 -0300

    publicserverurl log

--- apps/web/src/lib/server/auth-client.ts
+++ apps/web/src/lib/server/auth-client.ts
@@ -3,6 +3,7 @@ import { PUBLIC_SERVER_URL } from "$env/static/public";
 import { createAuthClient } from "better-auth/svelte";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 
+console.log("auth-client server: PUBLIC_SERVER_URL", PUBLIC_SERVER_URL);
 export const authServerClient = createAuthClient({
   baseURL: PUBLIC_SERVER_URL,
   // @ts-expect-error sveltekitCookies expects getRequestEvent to return a promise, but it doesn't
