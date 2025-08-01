eb7c61b9600bfa50bca5023f74835668e6359a72
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-08-01 00:34:57 -0300

    server side sessions

--- apps/web/src/routes/(protected)/+layout.svelte
+++ apps/web/src/routes/(protected)/+layout.svelte
@@ -2,24 +2,14 @@
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
   import AppSidebar from "$lib/components/app-sidebar.svelte";
   import SiteHeader from "$lib/components/site-header.svelte";
-  import { authClient } from "$lib/auth-client";
-  import { goto } from "$app/navigation";
 
-  const { children } = $props();
-
-  const session = authClient.useSession();
-
-  if (!session) {
-    goto("/sign-in");
-  }
+  const { children, data } = $props();
 </script>
 
 <Sidebar.Provider
   style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
 >
-  {#if $session.data}
-    <AppSidebar variant="inset" user={$session.data.user} />
-  {/if}
+  <AppSidebar variant="inset" user={data.user} />
   <Sidebar.Inset>
     <SiteHeader />
     {@render children()}
